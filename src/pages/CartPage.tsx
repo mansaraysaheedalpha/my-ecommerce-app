0cefbed4ddad1559c0be8e20e73c315562ab9c41
Saheed Alpha Mansaray <mansaraysaheedalpha@gmail.com>
2025-05-21 21:35:26 +0000

    Setup routing system using react router and some basic placeholders to get started

---
+++ src/pages/CartPage.tsx
@@ -0,0 +1,5 @@
+const Cart = () => {
+  return <h1>This is the CartPage page</h1>;
+};
+
+export default Cart;
---
+++ src/pages/ProductListPage.tsx
@@ -0,0 +1,5 @@
+const ProductList = () => {
+  return <h1>This is the ProductListPage page</h1>
+}
+
+export default ProductList;
